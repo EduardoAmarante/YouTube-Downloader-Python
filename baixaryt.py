9e410b43e56c87c243e006733851224caf1ad287
Eduardo Amarante <51537878+EduardoAmarante@users.noreply.github.com>
2022-04-24 20:23:22 -0300

    remove obsolete code

--- baixaryt.py
+++ baixaryt.py
@@ -3,12 +3,6 @@ from pytube.cli import on_progress
 import requests
 from bs4 import BeautifulSoup
 
-#url = input("Cole o link do seu video aqui:\n")
-url = "https://www.youtube.com/watch?v=wkX-7G0vF7s"
-#op = int(input("1 para audio e 2 para video:"))
-
-
-
 def baixaraudio(url):
     link = YouTube(url, on_progress_callback=on_progress)
     link.streams.get_audio_only(subtype="mp4").download()
@@ -17,23 +11,22 @@ def baixarvideo(url):
     link = YouTube(url, on_progress_callback=on_progress)
     link.streams.get_by_resolution("720p").download()
 
-""" if op != 1:
-    baixarvideo(url)
-else:
-    baixaraudio(url) """
-
 def Tags(url):
     link = YouTube(url, on_progress_callback=on_progress)
     r = link.keywords
     print(r)
 
+def getTITULO(url):
+    link = YouTube(url, on_progress_callback=on_progress)
+    r = link.title()
+    print(r)
 
+l = ['https://youtu.be/R2-0Rm8r1ik','https://youtu.be/8JhJPQHm7zA','https://youtu.be/fPDMHkS7p_w','https://youtu.be/cz-hbcVrVN8','https://youtu.be/gO2ISGyqtyM','https://youtu.be/rnjdmhUjkSo','https://www.youtube.com/watch?v=xUzeIy7EaOc','https://youtu.be/fsRUdiVznxc','https://youtu.be/mr1qPTxi0k0','https://youtu.be/_3MC8XZRnHs','https://youtu.be/ZnCxpsjFh78','https://youtu.be/haeB72i_UQU','https://youtu.be/dqrtyu6sPlE','https://youtu.be/-8mAxLSzSag','https://youtu.be/rHJIlSECIA8','https://youtu.be/YIjoOTSH3gY','https://youtu.be/NY8rWK4XRHQ','https://youtu.be/C060FJCs8dI','https://youtu.be/BOfQqJwew7I','https://youtu.be/VeTLQsAYSCI','https://youtu.be/BxVVNa3_nEg']
 
-#busca = Search("RATO BORRACHUDO")
-""" for i in range(0,3):
-    video = 'https://www.youtube.com/watch?v='+str(busca.results[i])[41:-1]
-    Tags(video) """
+print(len(l))
 
-#busca.results.sort(reverse=True)
+""" for i in l:
+    #getTITULO(i)
+    baixaraudio(i) """
 
-baixaraudio(url)
+baixaraudio('https://youtu.be/xUzeIy7EaOc')
