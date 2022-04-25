378d59ca9989c8ecc58ebf7a93220312962f01ac
Eduardo Amarante <51537878+EduardoAmarante@users.noreply.github.com>
2022-04-25 01:43:58 -0300

    trechos de teste removidos

--- baixaryt.py
+++ baixaryt.py
@@ -21,12 +21,8 @@ def getTITULO(url):
     r = link.title()
     print(r)
 
-l = ['https://youtu.be/R2-0Rm8r1ik','https://youtu.be/8JhJPQHm7zA','https://youtu.be/fPDMHkS7p_w','https://youtu.be/cz-hbcVrVN8','https://youtu.be/gO2ISGyqtyM','https://youtu.be/rnjdmhUjkSo','https://www.youtube.com/watch?v=xUzeIy7EaOc','https://youtu.be/fsRUdiVznxc','https://youtu.be/mr1qPTxi0k0','https://youtu.be/_3MC8XZRnHs','https://youtu.be/ZnCxpsjFh78','https://youtu.be/haeB72i_UQU','https://youtu.be/dqrtyu6sPlE','https://youtu.be/-8mAxLSzSag','https://youtu.be/rHJIlSECIA8','https://youtu.be/YIjoOTSH3gY','https://youtu.be/NY8rWK4XRHQ','https://youtu.be/C060FJCs8dI','https://youtu.be/BOfQqJwew7I','https://youtu.be/VeTLQsAYSCI','https://youtu.be/BxVVNa3_nEg']
 
-print(len(l))
+l = ['']
 
-""" for i in l:
-    #getTITULO(i)
-    baixaraudio(i) """
-
-baixaraudio('https://youtu.be/xUzeIy7EaOc')
+for i in l:
+    baixaraudio(i)
