6451cb47c84e8e9eb7ed69e47c5ae4f417d97cc1
Eduardo Amarante <eamarante80@gmail.com>
2022-06-07 02:40:42 -0300

    Update baixaryt.py

--- baixaryt.py
+++ baixaryt.py
@@ -11,7 +11,12 @@ def baixaraudio(url):
 
 def baixarvideo(url):
     link = YouTube(url, on_progress_callback=on_progress)
-    link.streams.get_by_resolution("720p").download()
+    
+    print('1080')
+    link.streams.filter(res="720p").first().download()
+
+    # print('720')
+    # link.streams.get_by_resolution("720p").download()
 
 def Tags(url):
     link = YouTube(url, on_progress_callback=on_progress)
@@ -23,9 +28,16 @@ def getTitulo(url):
     r = link.title
     return r
 
+ratoborrachudo = [
+'https://youtu.be/zWCvgOYerUU',
+'https://youtu.be/-0i_ZAIzQBE'
+'https://youtu.be/JRUTUcAX51A']
 
-l = ['https://www.youtube.com/watch?v=nny6kuchJ7o']
+# for video in ratoborrachudo:
+#     print(getTitulo(video))
+#     baixarvideo(video)
 
-for i in l:
-    baixaraudio(i)
+# baixarvideo('https://www.youtube.com/watch?v=akzT9gqw3Bk')
 
+baixaraudio('https://youtu.be/nny6kuchJ7o')
+baixaraudio('https://youtu.be/rD7_OB3Ua1w')
